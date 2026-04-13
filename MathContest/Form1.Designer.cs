9f7eaee9f1cc129c9db42f15bd611a60347243e1
Sturm <savagesturm@gmail.com>
2026-04-12 20:22:44 -0600

    Add project files.

---
+++ MathContest/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace MathContest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentInformatoinBox = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gradeBox = new System.Windows.Forms.TextBox();
+            this.ageBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.signSelctionbox = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.problemBox = new System.Windows.Forms.GroupBox();
+            this.secondNumbertext = new System.Windows.Forms.TextBox();
+            this.firstNumbertext = new System.Windows.Forms.TextBox();
+            this.firstNumberlabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.studentAnswer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.studentInformatoinBox.SuspendLayout();
+            this.signSelctionbox.SuspendLayout();
+            this.problemBox.SuspendLayout();
+            this.buttonBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // studentInformatoinBox
+            // 
+            this.studentInformatoinBox.AccessibleName = "StudentInfoBox";
+            this.studentInformatoinBox.Controls.Add(this.label3);
+            this.studentInformatoinBox.Controls.Add(this.label2);
+            this.studentInformatoinBox.Controls.Add(this.label1);
+            this.studentInformatoinBox.Controls.Add(this.gradeBox);
+            this.studentInformatoinBox.Controls.Add(this.ageBox);
+            this.studentInformatoinBox.Controls.Add(this.nameBox);
+            this.studentInformatoinBox.Location = new System.Drawing.Point(12, 12);
+            this.studentInformatoinBox.Name = "studentInformatoinBox";
+            this.studentInformatoinBox.Size = new System.Drawing.Size(304, 109);
+            this.studentInformatoinBox.TabIndex = 0;
+            this.studentInformatoinBox.TabStop = false;
+            this.studentInformatoinBox.Text = "Student Information";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Grade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // gradeBox
+            // 
+            this.gradeBox.AccessibleName = "GradeBox";
+            this.gradeBox.Location = new System.Drawing.Point(218, 40);
+            this.gradeBox.Name = "gradeBox";
+            this.gradeBox.Size = new System.Drawing.Size(44, 22);
+            this.gradeBox.TabIndex = 2;
+            // 
+            // ageBox
+            // 
+            this.ageBox.AccessibleName = "AgeBox";
+            this.ageBox.Location = new System.Drawing.Point(155, 40);
+            this.ageBox.Name = "ageBox";
+            this.ageBox.Size = new System.Drawing.Size(44, 22);
+            this.ageBox.TabIndex = 1;
+            // 
+            // nameBox
+            // 
+            this.nameBox.AccessibleName = "NameBox";
+            this.nameBox.Location = new System.Drawing.Point(6, 40);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 22);
+            this.nameBox.TabIndex = 0;
+            // 
+            // signSelctionbox
+            // 
+            this.signSelctionbox.AccessibleName = "SignSelectionBox";
+            this.signSelctionbox.Controls.Add(this.radioButton4);
+            this.signSelctionbox.Controls.Add(this.radioButton3);
+            this.signSelctionbox.Controls.Add(this.radioButton2);
+            this.signSelctionbox.Controls.Add(this.radioButton1);
+            this.signSelctionbox.Location = new System.Drawing.Point(11, 127);
+            this.signSelctionbox.Name = "signSelctionbox";
+            this.signSelctionbox.Size = new System.Drawing.Size(200, 175);
+            this.signSelctionbox.TabIndex = 1;
+            this.signSelctionbox.TabStop = false;
+            this.signSelctionbox.Text = "Math Problem Type";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(52, 129);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Division";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(49, 103);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(102, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Multipication";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(49, 77);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(88, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Subtration";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(52, 43);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Addition";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // problemBox
+            // 
+            this.problemBox.AccessibleName = "ProblemBox";
+            this.problemBox.Controls.Add(this.label4);
+            this.problemBox.Controls.Add(this.studentAnswer);
+            this.problemBox.Controls.Add(this.label5);
+            this.problemBox.Controls.Add(this.firstNumberlabel);
+            this.problemBox.Controls.Add(this.secondNumbertext);
+            this.problemBox.Controls.Add(this.firstNumbertext);
+            this.problemBox.Location = new System.Drawing.Point(217, 127);
+            this.problemBox.Name = "problemBox";
+            this.problemBox.Size = new System.Drawing.Size(200, 175);
+            this.problemBox.TabIndex = 2;
+            this.problemBox.TabStop = false;
+            this.problemBox.Text = "Math Problem";
+            // 
+            // secondNumbertext
+            // 
+            this.secondNumbertext.Location = new System.Drawing.Point(41, 90);
+            this.secondNumbertext.Name = "secondNumbertext";
+            this.secondNumbertext.Size = new System.Drawing.Size(100, 22);
+            this.secondNumbertext.TabIndex = 1;
+            // 
+            // firstNumbertext
+            // 
+            this.firstNumbertext.Location = new System.Drawing.Point(29, 45);
+            this.firstNumbertext.Name = "firstNumbertext";
+            this.firstNumbertext.Size = new System.Drawing.Size(100, 22);
+            this.firstNumbertext.TabIndex = 0;
+            // 
+            // firstNumberlabel
+            // 
+            this.firstNumberlabel.AutoSize = true;
+            this.firstNumberlabel.Location = new System.Drawing.Point(26, 26);
+            this.firstNumberlabel.Name = "firstNumberlabel";
+            this.firstNumberlabel.Size = new System.Drawing.Size(83, 16);
+            this.firstNumberlabel.TabIndex = 2;
+            this.firstNumberlabel.Text = "First Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Second Number";
+            // 
+            // buttonBox
+            // 
+            this.buttonBox.AccessibleName = "ButtonBox";
+            this.buttonBox.Controls.Add(this.button4);
+            this.buttonBox.Controls.Add(this.button3);
+            this.buttonBox.Controls.Add(this.button2);
+            this.buttonBox.Controls.Add(this.button1);
+            this.buttonBox.Location = new System.Drawing.Point(452, 33);
+            this.buttonBox.Name = "buttonBox";
+            this.buttonBox.Size = new System.Drawing.Size(336, 360);
+            this.buttonBox.TabIndex = 3;
+            this.buttonBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(131, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Summery";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(131, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // studentAnswer
+            // 
+            this.studentAnswer.AccessibleName = "StudentAnswer";
+            this.studentAnswer.Location = new System.Drawing.Point(43, 147);
+            this.studentAnswer.Name = "studentAnswer";
+            this.studentAnswer.Size = new System.Drawing.Size(100, 22);
+            this.studentAnswer.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Student Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonBox);
+            this.Controls.Add(this.problemBox);
+            this.Controls.Add(this.signSelctionbox);
+            this.Controls.Add(this.studentInformatoinBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.studentInformatoinBox.ResumeLayout(false);
+            this.studentInformatoinBox.PerformLayout();
+            this.signSelctionbox.ResumeLayout(false);
+            this.signSelctionbox.PerformLayout();
+            this.problemBox.ResumeLayout(false);
+            this.problemBox.PerformLayout();
+            this.buttonBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox studentInformatoinBox;
+        private System.Windows.Forms.TextBox gradeBox;
+        private System.Windows.Forms.TextBox ageBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox signSelctionbox;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox problemBox;
+        private System.Windows.Forms.TextBox secondNumbertext;
+        private System.Windows.Forms.TextBox firstNumbertext;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label firstNumberlabel;
+        private System.Windows.Forms.GroupBox buttonBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox studentAnswer;
+    }
+}
+
---
+++ MathContest/Form1.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * [] Make the name, grade, and age textbox appear yellow
+ * [] Limit grade 1-4 appear yellow when out of range
+ * [] Limit age 7-11 appear yellow when out of range
+ * [] Lock Submit button until all fields are filled ( grade, name, answer, and age)
+ * [] Lock the Summary button until the submit button has been clicked once
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathContest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
